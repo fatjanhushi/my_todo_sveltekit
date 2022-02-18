fe6d059629050f4431b1e62cd9367d700734c4a7
Fatjan Hushi <me@fatjoni.com>
2022-02-18 17:55:56 +0100

    Fixed typo on index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,2 @@
 <h1>Welcome to the Todo App</h1>
-<h3>Please <a href="/auth/login">Log In</a> or <a href="/auth/register">Register an account</a> to get use this app</h3>
+<h3>Please <a href="/auth/login">Log In</a> or <a href="/auth/register">Register an account</a> to use this app with your custom Todo's</h3>
